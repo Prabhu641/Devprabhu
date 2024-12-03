4b85a5db6160f870dea5e4861d743725e75ae38a
Prabhu R <prabhurprabhur063@gmail.com>
2024-12-03 09:57:55 +0530

    updated new skills

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -6,6 +6,7 @@ function Skills() {
         <div className="skills-page">
             <div className="header text-center mb-5">
                 <h3 className='text-white'>Technical <span className='text-info'>Skills</span></h3>
+                <h1>Tools</h1>
        
             </div>
 
@@ -21,6 +22,7 @@ function Skills() {
                             </div>
                         ))}
                     </div>
+                    
                     
                 </div>
 
