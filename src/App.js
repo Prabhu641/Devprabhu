351caa0eab11a6babe6704b089f4ac0c52242f8a
Prabhu R <prabhurprabhur063@gmail.com>
2024-12-12 19:16:02 +0530

    now updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {  Route, Routes } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home'; 
 import About from './Components/About'; 
@@ -7,21 +7,43 @@ import { Contact } from './Components/Contact';
 import { Certifcates } from './Components/Certifcates';
 import Skills from './Components/Skills';
 import { Project } from './Components/Project';
+import LoadingScreen from 'react-loading-screen';
 
 function App() {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
+
+        return () => clearTimeout(timer); 
+    }, []);
+
     return (
         <>
             <Navbar />
             
-              <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/skills" element={<Skills />} />
-                    <Route path="/project" element={<Project />} />
-                    <Route path="/certificates" element={<Certifcates/>} />
-                    <Route path="/contact" element={<Contact />} />
-                </Routes>
-          
+            <LoadingScreen
+                loading={isLoading}
+                bgColor='#000000'
+                spinnerColor='#ffffff'
+                textColor='#676767'
+                text='DevPrabhu is Loading...'
+            >
+               
+                {!isLoading && (
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/skills" element={<Skills />} />
+                        <Route path="/project" element={<Project />} />
+                        <Route path="/certificates" element={<Certifcates />} />
+                        <Route path="/contact" element={<Contact />} />
+                    </Routes>
+                )}
+            </LoadingScreen>
         </>
     );
 }
---
+++ src/Components/LoadingScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const LoadingScreen = () => {
+    return (
+        <div className="loading-screen">
+            <div className="loader"></div>
+            <h1>Loading DevPrabhu...</h1>
+        </div>
+    );
+};
+
+export default LoadingScreen;
