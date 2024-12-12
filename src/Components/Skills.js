0f0d47cec202b746f9a20836ae99505e8d8fb871
Prabhu R <prabhurprabhur063@gmail.com>
2024-12-12 18:50:34 +0530

    fix:skills page

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -10,35 +10,40 @@ function Skills() {
             once: true, 
         });
     }, []);
+
+    // Array of skills with image sources
+    const skills = [
+        { name: 'HTML', img: './html.png' },
+        { name: 'CSS', img: './css.png' },
+        { name: 'JavaScript', img: './javascript.png' },
+        { name: 'React', img: './react.png' },
+        { name: 'Bootstrap', img: './bootstrap.png' },
+        { name: 'TypeScript', img: './typescript.png' },
+        { name: 'jQuery', img: './jquery.png' },
+        { name: 'Node.js', img: './nodejs.png' },
+        { name: 'Express', img: './express.png' },
+        { name: 'MongoDB', img: './mongodb.png' },
+        { name: 'Python', img: './python.png' },
+        { name: 'Django', img: './django.png' }
+    ];
+
     return (
-        <div className="skills-page" data-aos="fade-right" >
+        <div className="skills-page" data-aos="fade-right">
             <div className="header text-center mb-5">
                 <h3 className='text-white'>Technical <span className='text-info'>Skills</span></h3>
-              
-
-       
             </div>
 
             <div className="skills-containers">
-        
                 <div className="skills-category">
-             
                     <div className="skills-list" data-aos="fade-right">
-                        {['HTML', 'CSS', 'JavaScript', 'React', 'Bootstrap', 'TypeScript','jquery','Nodejs', 'Express', 'MongoDB', 'Python', 'Django'].map((skill) => (
-                            <div className="skill-card"  key={skill}>
-                                <img src={`./${skill.toLowerCase()}.png`} alt={skill} className="skill-icon" />
-                                <span className="skill-name">{skill}</span>
+                        {skills.map((skill) => (
+                            <div className="skill-card" key={skill.name}>
+                                <img src={skill.img} alt={skill.name} className="skill-icon" />
+                                <span className="skill-name">{skill.name}</span>
                             </div>
                         ))}
                     </div>
-                    
-                    
                 </div>
-
-
-        
-                  
-           
             </div>
         </div>
     );
