ea8f23746816245e941207344ff0b9f479fb188e
Prabhu R <prabhurprabhur063@gmail.com>
2025-01-04 00:05:20 +0530

    last update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,25 +9,29 @@ const Footer = () => {
       
         <hr/>
         <footer className="footer">
+           
             <div className="footer-links">
-             <span>© DevPrabhu 2024. All rights reserved.</span>
+                 <div className="footer-header">
+                       <h5>© DevPrabhu 2024. All rights reserved.</h5>
+                </div>
+
                 <Link className="footer-link" to="/">
-                    <i className="fas fa-home"></i> Home
+                    <i className="fas fa-home"></i> 
                 </Link>
                 <Link className="footer-link" to="/about">
-                    <i className="fas fa-user"></i> About
+                    <i className="fas fa-user"></i> 
                 </Link>
                 <Link className="footer-link" to="/skills">
-                    <i className="fas fa-cogs"></i> Skills
+                    <i className="fas fa-cogs"></i> 
                 </Link>
                 <Link className="footer-link" to="/project">
-                    <i className="fas fa-briefcase"></i> Project
+                    <i className="fas fa-briefcase"></i> 
                 </Link>
                 <Link className="footer-link" to="/certificates">
-                    <i className="fas fa-certificate"></i> Certificates
+                    <i className="fas fa-certificate"></i> 
                 </Link>
                 <Link className="footer-link" to="/contact">
-                    <i className="fas fa-envelope"></i> Contact
+                    <i className="fas fa-envelope"></i> 
                 </Link>
             </div>
         </footer>
